a8060d0f07f58ae74edfe65ce2f06c237b3272e1
Rocio <rocioalonso.dev@gmail.com>
2023-06-26 11:11:59 +0200

    api github users

---
+++ script.js
@@ -0,0 +1,30 @@
+const APIURL = 'https://api.github.com/users/';
+const searchInput = document.getElementById('search');
+
+const avatar = document.getElementById('avatar');
+const name = document.getElementById('name');
+const followers = document.getElementById('followers');
+const repos = document.getElementById('repos');
+const profileLink = document.getElementById('profile-link');
+
+
+
+const request = async() => {
+    let username = searchInput.value
+        try{
+            const response = await axios.get(APIURL+username)
+            const usuario = response.data;
+            console.log(usuario)
+            avatar.src=usuario.avatar_url;
+            name.innerHTML = usuario.login;
+            followers.innerHTML = usuario.followers + " FOLLOWERS";
+            repos.innerHTML = usuario.public_repos + " REPOSITORIES";
+            profileLink.innerHTML = usuario.html_url
+
+            
+        }catch(error){
+            console.log(error)
+        }
+}
+
+searchInput.addEventListener('change',request)
