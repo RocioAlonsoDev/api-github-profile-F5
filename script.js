4f76e72db4269cb9dc6e8a85d35d05a8b57df83b
Rocio <rocioalonso.dev@gmail.com>
2023-06-28 11:29:13 +0200

    api github profile script finished

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const APIURL = 'https://api.github.com/users/';
 const searchInput = document.getElementById('search');
 
+const main = document.getElementById('main');
 const avatar = document.getElementById('avatar');
 const name = document.getElementById('name');
 const followers = document.getElementById('followers');
 const repos = document.getElementById('repos');
 const profileLink = document.getElementById('profile-link');
 
-
+main.style.display = 'none'
 
 const request = async() => {
     let username = searchInput.value
         try{
             const response = await axios.get(APIURL+username)
             const usuario = response.data;
-            console.log(usuario)
+            main.style.display = 'block'
             avatar.src=usuario.avatar_url;
             name.innerHTML = usuario.login;
             followers.innerHTML = usuario.followers + " FOLLOWERS";
